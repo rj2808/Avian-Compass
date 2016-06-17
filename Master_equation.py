7990f74d292d1332c3c203dbdc2f30b45bfebe57
Rakshit <Rakshit@Rakshits-MacBook-Pro.local>
2016-06-17 15:58:41 -0700

    Adding an Example

--- Master_equation.py
+++ Master_equation.py
@@ -25,3 +25,36 @@ def qubit_integrate(epsilon, delta, g1, g2, solver):
  	return output.expect[1], output.expect[2], output.expect[3]
 
 
+
+
+
+epsilon = 0.0 * 2 * num.pi
+delta	 = 1.0 * 2 * num.pi
+g2 = .15
+g1 = 0.0
+
+# intial state
+psi0 = basis(2,0) # In State Up
+tlist = num.linspace(0,5,200)
+
+#analytics
+sx_analytic = num.zeros(shape(tlist))
+sy_analytic = -num.sin(2*np.pi*tlist) * num.exp(-tlist * g2)
+sz_analytic = num.cos(2*np.pi*tlist) * num.exp(-tlist * g2)
+
+
+fig, ax = plt.subplots(figsize=(12,6))
+ax.plot(tlist, num.real(sx1), 'r')
+ax.plot(tlist, num.real(sy1), 'b')
+ax.plot(tlist, num.real(sz1), 'g')
+ax.plot(tlist, sx_analytic, 'r*')
+ax.plot(tlist, sy_analytic, 'g*')
+ax.plot(tlist, sz_analytic, 'g*')
+ax.legend(("sx", "sy", "sz"))
+ax.set_xlabel('Time')
+ax.set_ylabel('expectation value');
+
+
+
+
+
