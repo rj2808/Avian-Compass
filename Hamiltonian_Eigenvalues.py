b89ab2b11496cf804af810f412dff07f1780462c
rj2808 <rakshit28081996@gmail.com>
2016-09-17 15:44:02 +0530

    Some Readiblity changes

--- Hamiltonian_Eigenvalues.py
+++ Hamiltonian_Eigenvalues.py
@@ -50,7 +50,7 @@ def Hamiltonian_eigenvalues(theta1, theta2, phi1, phi2): # Here o refers to the 
 	T_H7n = np.array([[.2540, 0, 0],[0, -.0594, 0], [0, 0, -.1945]])
 	T_H7 =  qt.Qobj(T_H7n)
 	T_Hb1n = np.array([[0.1521, 0, 0], [0, -.0456, 0], [0, 0, -.1065]])
-	T_Hb1 = qt.Qobj(T_Hb1)
+	T_Hb1 = qt.Qobj(T_Hb1n)
 
 	# Now since we have defined all the tensors required till now, Writing down the hamiltonian
 	# H_a denotes the hamiltonian for FAD Radical
@@ -59,13 +59,28 @@ def Hamiltonian_eigenvalues(theta1, theta2, phi1, phi2): # Here o refers to the 
 	Ha_zeeman_nt = omega * (Sx * mt.sin(theta1) *  mt.cos(phi1) + Sy *  mt.sin(theta1) * mt. sin(phi1) + Sz * mt.cos(theta1))			# Hamiltonian without any tensor product
 	Ha_zeeman = qt.tensor(Ha_zeeman_nt, qt.identity(3**7))			# Compounding different states 
 	# Using the anisotropic hyperfine tensors the hamiltonian comes down to this
-	Ha_hfi_N5  = qt.tensor(a_iso1[0] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_N5 * I2_x) + qt.tensor(Sy, T_N5 * I2_y) + qt.tensor(Sz, T_N5 * I2_z), qt.identity(3**6))
-	Ha_hfi_N10 = qt.tensor( qt.tensor(3**1), a_iso1[1] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_N10 * I2_x) + qt.tensor(Sy, T_N10 * I2_y) + qt.tensor(Sz, T_N10 * I2_z), qt.identity(3**5))
-	Ha_hfi_H6  = qt.tensor( qt.tensor(3**2), a_iso1[2] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H6 * I2_x) + qt.tensor(Sy, T_H6 * I2_y) + qt.tensor(Sz, T_H6 * I2_z), qt.identity(3**4))
-	Ha_hfi_H8  = qt.tensor( qt.tensor(3**3), a_iso1[3] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H8 * I2_x) + qt.tensor(Sy, T_H8 * I2_y) + qt.tensor(Sz, T_H8 * I2_z), qt.identity(3**3))
-	Ha_hfi_b1  = qt.tensor( qt.tensor(3**4), a_iso1[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_Hb * I2_x) + qt.tensor(Sy, T_Hb * I2_y) + qt.tensor(Sz, T_Hb * I2_z), qt.identity(3**2))
-	Ha_hfi_b2  = qt.tensor( qt.tensor(3**5), a_iso1[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_Hb * I2_x) + qt.tensor(Sy, T_Hb * I2_y) + qt.tensor(Sz, T_Hb * I2_z), qt.identity(3**1))
-	Ha_hfi_b3  = qt.tensor( qt.tensor(3**6), a_iso1[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_Hb * I2_x) + qt.tensor(Sy, T_Hb * I2_y) + qt.tensor(Sz, T_Hb * I2_z), qt.identity(3**0))
+	Ha_hfi_N5  = qt.tensor(a_iso1[0] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+	 				+ qt.tensor(Sx, T_N5 * I2_x) + qt.tensor(Sy, T_N5 * I2_y) + qt.tensor(Sz, T_N5 * I2_z), qt.identity(3**6))
+
+	Ha_hfi_N10 = qt.tensor( qt.tensor(3**1), a_iso1[1] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_N10 * I2_x) + qt.tensor(Sy, T_N10 * I2_y) + qt.tensor(Sz, T_N10 * I2_z), qt.identity(3**5))
+
+	Ha_hfi_H6  = qt.tensor( qt.tensor(3**2), (a_iso1[2] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z))) 
+					+  qt.tensor(Sx, T_H6 * I2_x) + qt.tensor(Sy, T_H6 * I2_y) + qt.tensor(Sz, T_H6 * I2_z), qt.identity(3**4))
+
+	Ha_hfi_H8  = qt.tensor( qt.tensor(3**3), a_iso1[3] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_H8 * I2_x) + qt.tensor(Sy, T_H8 * I2_y) + qt.tensor(Sz, T_H8 * I2_z), qt.identity(3**3))
+
+	Ha_hfi_b1  = qt.tensor( qt.tensor(3**4), a_iso1[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_Hb * I2_x) + qt.tensor(Sy, T_Hb * I2_y) + qt.tensor(Sz, T_Hb * I2_z), qt.identity(3**2))
+
+	Ha_hfi_b2  = qt.tensor( qt.tensor(3**5), a_iso1[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_Hb * I2_x) + qt.tensor(Sy, T_Hb * I2_y) + qt.tensor(Sz, T_Hb * I2_z), qt.identity(3**1))
+
+	Ha_hfi_b3  = qt.tensor( qt.tensor(3**6), a_iso1[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_Hb * I2_x) + qt.tensor(Sy, T_Hb * I2_y) + qt.tensor(Sz, T_Hb * I2_z), qt.identity(3**0))
+
+
 
 	H_a = Ha_zeeman + Ha_hfi_N5 + Ha_hfi_N10 + Ha_hfi_H6 + Ha_hfi_H8 + Ha_hfi_b1 + Ha_hfi_b2 + Ha_hfi_b3
 	H_a_eigen = H_a.eigenenergies()
@@ -74,16 +89,30 @@ def Hamiltonian_eigenvalues(theta1, theta2, phi1, phi2): # Here o refers to the 
 	Hb_zeeman_nt = omega * (Sx * mt.sin(theta2) *  mt.cos(phi2) + Sy *  mt.sin(theta2) * mt. sin(phi2) + Sz * mt.cos(theta2))			# Hamiltonian without any tensor product
 	Ha_zeeman = qt.tensor(Hb_zeeman_nt, qt.identity(3**7))			# Compounding different states 
 	# Using the anisotropic hyperfine tensors the hamiltonian comes down to this
-	Hb_hfi_N1  = qt.tensor(a_iso2[0] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_N1 * I2_x) + qt.tensor(Sy, T_N1 * I2_y) + qt.tensor(Sz, T_N1 * I2_z), qt.identity(3**6))
-	Hb_hfi_H1  = qt.tensor( qt.tensor(3**1), a_iso2[1] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H1 * I2_x) + qt.tensor(Sy, T_H1 * I2_y) + qt.tensor(Sz, T_H1 * I2_z), qt.identity(3**5))
-	Hb_hfi_H2  = qt.tensor( qt.tensor(3**2), a_iso2[2] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H2 * I2_x) + qt.tensor(Sy, T_H2 * I2_y) + qt.tensor(Sz, T_H2 * I2_z), qt.identity(3**4))
-	Hb_hfi_H4  = qt.tensor( qt.tensor(3**3), a_iso2[3] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H4 * I2_x) + qt.tensor(Sy, T_H4 * I2_y) + qt.tensor(Sz, T_H4 * I2_z), qt.identity(3**3))
-	Hb_hfi_H5  = qt.tensor( qt.tensor(3**4), a_iso2[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H5 * I2_x) + qt.tensor(Sy, T_H5 * I2_y) + qt.tensor(Sz, T_H5 * I2_z), qt.identity(3**2))
-	Hb_hfi_H7  = qt.tensor( qt.tensor(3**5), a_iso2[5] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_H7 * I2_x) + qt.tensor(Sy, T_H7 * I2_y) + qt.tensor(Sz, T_H7 * I2_z), qt.identity(3**1))
-	Hb_hfi_Hb1 = qt.tensor( qt.tensor(3**6), a_iso2[6] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) +  qt.tensor(Sx, T_Hb1 * I2_x) + qt.tensor(Sy, T_Hb1 * I2_y) + qt.tensor(Sz, T_Hb1 * I2_z), qt.identity(3**0))
+	Hb_hfi_N1  = qt.tensor(a_iso2[0] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_N1 * I2_x) + qt.tensor(Sy, T_N1 * I2_y) + qt.tensor(Sz, T_N1 * I2_z), qt.identity(3**6))
+
+	Hb_hfi_H1  = qt.tensor( qt.tensor(3**1), a_iso2[1] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_H1 * I2_x) + qt.tensor(Sy, T_H1 * I2_y) + qt.tensor(Sz, T_H1 * I2_z), qt.identity(3**5))
+
+	Hb_hfi_H2  = qt.tensor( qt.tensor(3**2), a_iso2[2] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_H2 * I2_x) + qt.tensor(Sy, T_H2 * I2_y) + qt.tensor(Sz, T_H2 * I2_z), qt.identity(3**4))
+
+	Hb_hfi_H4  = qt.tensor( qt.tensor(3**3), a_iso2[3] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_H4 * I2_x) + qt.tensor(Sy, T_H4 * I2_y) + qt.tensor(Sz, T_H4 * I2_z), qt.identity(3**3))
+
+	Hb_hfi_H5  = qt.tensor( qt.tensor(3**4), a_iso2[4] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_H5 * I2_x) + qt.tensor(Sy, T_H5 * I2_y) + qt.tensor(Sz, T_H5 * I2_z), qt.identity(3**2))
+
+	Hb_hfi_H7  = qt.tensor( qt.tensor(3**5), a_iso2[5] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_H7 * I2_x) + qt.tensor(Sy, T_H7 * I2_y) + qt.tensor(Sz, T_H7 * I2_z), qt.identity(3**1))
+
+	Hb_hfi_Hb1 = qt.tensor( qt.tensor(3**6), a_iso2[6] * (qt.tensor(Sx, I2_x) + qt.tensor(Sy, I2_y) + qt.tensor(Sz, I2_z)) 
+					+  qt.tensor(Sx, T_Hb1 * I2_x) + qt.tensor(Sy, T_Hb1 * I2_y) + qt.tensor(Sz, T_Hb1 * I2_z), qt.identity(3**0))
 
 	H_b = Hb_zeeman + Hb_hfi_N1 + Hb_hfi_H1 + Hb_hfi_H2 + Hb_hfi_H4 + Ha_hfi_H5 + Hb_hfi_H7 + Hb_hfi_Hb1
 	H_b_eigen = H_b.eigenenergies()
+	
 	print H_b_eigen
 	print H_a_eigen
 
